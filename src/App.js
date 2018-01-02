7c6b10ddaf3811a30dde7e480eeb59aef3011236
Sergey Boychuk <sergeboychuk@gmail.com>
2018-01-01 21:40:27 -0500

    Changed the api to use amazon web services affiliate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import Button from './Components/Button'
-import Input from './Components/Input'
+import Button from './Components/Button';
+import Input from './Components/Input';
+require('./styles/css/App.css');
 
 const Key = require('./API_KEYS/Key');
 
 
-const Amazon = require('amazon-book-search');
-const amazonClient = new Amazon((Key.keys[0].API_KEY, Key.keys[0].SECRET_KEY));
+const amazon = require('amazon-product-api');
 
 
 
-require('./styles/css/App.css');
+const client = amazon.createClient({
+  awsId: Key.keys[0].API_KEY,
+  awsSecret: Key.keys[0].SECRET_KEY,
+  awsTag: "sergeyboychuk-20"
+})
+
+
+
+
+
+
+
 
 
 class App extends Component {
@@ -25,25 +36,74 @@ class App extends Component {
     }
   }
 
+  // fetchBookByISBN = (isbn) =>{
+  //   const filteredIsbn = isbn.replace(/[^\w\s]/gi, '')
+  //   fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + filteredIsbn)
+  //   .then(results => {
+  //     return results.json();
+  //   }).then(data => {
+  //     console.log(data.items[0].volumeInfo.title);
+  //     let books = data.items.map((book) => {
+  //       return (
+  //         <div className="bookResults" key={data.items}>
+  //           <img src={book.volumeInfo.imageLinks.thumbnail}/>
+  //           <p>{book.volumeInfo.title}</p>
+  //         </div>
+  //       )
+  //     })
+  //     this.setState({books: books});
+  //     console.log("state", this.state.books)
+      
+  //   })
+  // }
+
   fetchBookByISBN = (isbn) =>{
     const filteredIsbn = isbn.replace(/[^\w\s]/gi, '')
-    fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + filteredIsbn)
-    .then(results => {
-      return results.json();
-    }).then(data => {
-      console.log(data.items[0].volumeInfo.title);
-      let books = data.items.map((book) => {
-        return (
-          <div className="bookResults" key={data.items}>
-            <img src={book.volumeInfo.imageLinks.thumbnail}/>
-            <p>{book.volumeInfo.title}</p>
-          </div>
-        )
-      })
-      this.setState({books: books});
-      console.log("state", this.state.books)
-      
-    })
+    client.itemLookup({
+      idType: 'ISBN',
+      itemId: filteredIsbn,
+      responseGroup: 'ItemAttributes,Offers,Images'
+    }, (err, results, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let books = results[0].DetailPageURL.map((book) => {
+          console.log(book);
+        });
+        console.log(books);
+        
+
+        this.setState({booksISBN: books})
+        // let eachBook = books.map((key, value) => {
+        //   console.log(key, value)
+        // })
+        // let mappedBooks = Array.prototype.books.forEach((book) => {
+        //   return (
+        //     <div>
+        //       <h1>One book</h1>
+        //     </div>
+        //   )
+        // })
+        
+        
+        
+        
+        // return (
+        //   <div className="bookResults">
+        //     <p>Hello</p>
+        //   </div>
+            
+        // )
+  
+        // return (
+        //   <div className="bookResults">
+        //     <p>{books[0].ItemAttributes[0].Title}</p>
+        //   </div>
+        // )
+        // console.log(books[0].ItemAttributes[0].Title)
+      }
+      console.log(this.state.booksISBN)
+    });
   }
 
   fetchBookByName = (bookName) => {
